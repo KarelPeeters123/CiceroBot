e2421adc31834c09308314a9255e884ae856cd66
Karel <Karel.Peeters@ciminko.lu>
2020-11-15 08:54:54 +0100

    fixed empty string issue in dynamodb

--- src/main/java/lu/karelpeeters/Discordbot/model/Candidate.java
+++ src/main/java/lu/karelpeeters/Discordbot/model/Candidate.java
@@ -7,9 +7,37 @@ public class Candidate {
 	public String profileURL;
 
 	public Candidate(String id, String discordName, String currentNickname, String profileURL) {
+		this.setId(id);
+		this.setDiscordName(discordName);
+		this.setCurrentNickname(currentNickname);
+		this.setProfileURL(profileURL);
+	}
+
+	public void setId(String id) {
+		if (id.isEmpty()) {
+			throw new IllegalArgumentException("Must have a valid ID");
+		}
 		this.id = id;
+	}
+
+	public void setDiscordName(String discordName) {
+		if (discordName.isEmpty()) {
+			this.discordName = "N/A";
+		}
 		this.discordName = discordName;
+	}
+
+	public void setCurrentNickname(String currentNickname) {
+		if (currentNickname.isEmpty()) {
+			this.currentNickname = "N/A";
+		}
 		this.currentNickname = currentNickname;
+	}
+
+	public void setProfileURL(String profileURL) {
+		if (profileURL.isEmpty()) {
+			this.profileURL = "N/A";
+		}
 		this.profileURL = profileURL;
 	}
 }
